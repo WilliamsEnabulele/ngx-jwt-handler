bcb8d748eb46030a573d0a29ba5d57f9b54038e0
Williams Enabulele <enabsdigital@gmail.com>
2024-03-21 05:07:01 +0100

    make services public

---
+++ projects/ngx-jwt-handler/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-jwt-handler
+ */
+
+export * from './lib/jwt-handler.service';
+export * from './lib/session-handler.service';
+export * from './lib/token-interceptor.service';
